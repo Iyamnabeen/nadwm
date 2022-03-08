8a0b5b495a104decc5dfa60798065feeb93dd0b6
Nabeen Tiwaree <90331517+Iyamnabeen@users.noreply.github.com>
2022-03-08 10:44:27 +0545

    added script!

---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+git clone https://github.com/Iyamnabeen/nadwm.git && cd nadwm/dwm && sudo make clean install && cd ../dmenu && sudo make clean install && cd ../slstatus && sudo make clean install && cd ../slstatus && sudo make clean install && cd ../st && sudo make clean install
