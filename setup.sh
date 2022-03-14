07e0558d161d752d7bdb5d85f43569bbeb3c50b9
Nabeen Tiwaree <90331517+Iyamnabeen@users.noreply.github.com>
2022-03-14 10:15:47 +0545

    Updated set!

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+
 #!/bin/bash
 
 git clone --depth=1 https://github.com/Iyamnabeen/nadwm.git && cd nadwm/dwm && sudo make clean install && cd ../dmenu && sudo make clean install && cd ../slstatus && sudo make clean install && cd ../slstatus && sudo make clean install && cd ../st && sudo make clean install
+
